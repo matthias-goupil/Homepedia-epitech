94766a1d1ac96849cb274d4581e6d074ae2617f1
DuquesnoyPaul <paulduquesnoy@outlook.fr>
2025-06-25 16:17:45 +0200

    fix start master

--- bigdata-communes/spark/start-master.sh
+++ bigdata-communes/spark/start-master.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/opt/spark/sbin/start-master.sh
+/opt/spark/bin/spark-class org.apache.spark.deploy.master.Master --host spark-master --port 7077 --webui-port 8080
 export PATH=$PATH:/opt/spark/bin
 tail -f /dev/null
